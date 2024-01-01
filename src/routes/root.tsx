4f9649dd459637771c959ce8fdb58a60b19f58b4
httpolar <http@polar.moe>
2024-01-01 19:06:16 +0400

    Added header to the root layout

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner.tsx";
+import { Header } from "@/components/header.tsx";
 
 export const RootRoute: FC = () => {
   return (
     <>
+      <Header />
       <Outlet />
       <Toaster />
     </>
