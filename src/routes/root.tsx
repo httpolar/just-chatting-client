9c40608bab23ce1e82e245fd19273619e9f68edd
httpolar <http@polar.moe>
2024-01-15 22:37:09 +0400

    Fetch user data when the webapp is opened

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,16 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner.tsx";
 import { Header } from "@/components/header.tsx";
+import { useUserStore } from "@/stores/user.ts";
 
 export const RootRoute: FC = () => {
+  const userStore = useUserStore();
+
+  useEffect(() => {
+    void userStore.getUser();
+  }, []);
+
   return (
     <>
       <Header />
