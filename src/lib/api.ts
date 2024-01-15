d63f93f18ab507b60a09893c7725b5f1a530ca5f
httpolar <http@polar.moe>
2024-01-16 01:34:47 +0400

    Message history request function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -20,3 +20,22 @@ export const register = async (username: string, password: string) => {
     },
   });
 };
+
+/*
+*     val room: UInt = 0u,
+    val beforeTimestamp: Instant? = null,
+    val beforeMessageUuid: @Contextual UUID? = null
+* */
+
+export const messagesHistory = async (room: string, token: string) => {
+  const url = new URL(`${API_URL}/messages`);
+  url.searchParams.set("room", room);
+
+  const headers = new Headers();
+  headers.set("Authorization", `Bearer ${token}`);
+
+  return await fetch(url, {
+    method: "GET",
+    headers,
+  });
+};
